9a476e196955c673784a8e4922b914281f4b80d8
Indykatz <cats83@me.com>
2022-06-08 09:57:02 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import HeaderBanner from "./component/Header";
+import FooterBanner from "./component/Footer";
 
 function App() {
+  // list
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <div className="header">
+        <HeaderBanner
+          name="Katherine Ayers"
+          masters="38"
+          description="Calculator"
+        />
+      </div>
+      {/* Body */}
+      <div className="content"></div>
+      {/* Footer */}
+      <div className="footer">
+        <FooterBanner name="Katherine Ayers" github="Indykatz" />
+      </div>
+      {/* End of App div */}
     </div>
   );
 }
