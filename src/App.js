f87ec483f337007afea4f69a3f029cb105b7af43
Indykatz <cats83@me.com>
2022-06-13 09:23:53 +0100

    fixed header

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
 
   return (
     <div className="App">
-      Header
+      {/* Header */}
       <div className="header">
         <HeaderBanner
           name="Katherine Ayers"
